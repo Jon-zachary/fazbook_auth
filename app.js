bf8521a660c027429ca33fad0c3fffadb636f341
Jon Zachary <jontzachary@gmail.com>
2017-01-28 15:45:04 -0500

    add express-seesion,passport and new routes

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // add new modules and files here
+//adds the session and passport middleware and
+//adds the appropriate route handler paths. 
+//also creates an instance of express called app.
+const session = require('express-session');
+const passport = require('passport');
+
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth.js');
+const userRoutes = require('./routes/user.js');
+const app = express();
+
+// load environment variables
+//allows us to store sensitive info in enviroment variables
+//which we can add to our git ignore file to keep them safe.
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,11 +32,23 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // add new express-session and passport middleware here
-
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUnitialized: true,
+}));
+//adds passport and express session middleware, 
+//having express-session session and passport.session seems
+//like it could leaqd to tears. also which one does what?
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add route middleware here
-
+// adds routes as authRoutes and userRoutes
+app.use('/',index);
+app.use('/auth',authRoutes);
+app.use('/user',userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
