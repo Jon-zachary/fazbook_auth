9e431b82a4002a35eb24c3a0e3ebedbad93979dd
Jon Zachary <jontzachary@gmail.com>
2017-01-29 13:04:58 -0500

    add auth routes

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -9,3 +9,31 @@ const models = require('../db/models/index');
 function comparePass(userPassword,databasePassword) {
   return bcrypt.compareSync(userPassword,databasePassword);
 }
+
+//function will return a 401 status error if a user tries to log in
+//while already logged in, otherwise will neturn next which will execute the next 
+//piece of code from where it was called.
+function loginRedirect(req,res,next) {
+  if(req.user) return res.status(401).json(
+    { status: 'You are already logged in' });
+    return next();
+}
+
+//function will create a user, using the sequelize
+//models.create method. It also encrypts and salts the
+//hash using bcrypt. after it's done it redirects to '/'
+function createUser(req,res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dob: req.body.dob
+  }).then(() => {
+    res.redirect('/');
+  });
+}
+
